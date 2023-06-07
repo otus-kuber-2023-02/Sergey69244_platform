a892b43df5fc95acb92e6df1a968a52e3b63a5dc
Sergey Abramov <sergey3003@mail.ru>
2023-06-07 13:13:59 +0300

    kubernetes-operator Part I

---
+++ kubernetes-operators/clear-all.sh
@@ -0,0 +1,5 @@
+kubectl delete mysqls.otus.homework mysql-instance
+kubectl delete deployments.apps mysql-instance
+kubectl delete pvc mysql-instance-pvc
+kubectl delete pv mysql-instance-pv
+kubectl delete svc mysql-instance
