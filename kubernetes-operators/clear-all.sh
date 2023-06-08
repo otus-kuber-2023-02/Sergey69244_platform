b8e49ae3e95984d7f70f2eb058f8780e429f8138
Sergey Abramov <sergey3003@mail.ru>
2023-06-08 18:35:40 +0300

    Homework #3 kubernetes-networks

--- kubernetes-operators/clear-all.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete mysqls.otus.homework mysql-instance
-kubectl delete deployments.apps mysql-instance
-kubectl delete pvc mysql-instance-pvc
-kubectl delete pv mysql-instance-pv
-kubectl delete svc mysql-instance
